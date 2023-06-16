5578b185187f31c3ff611955617bcd84d0e4bf1f
AlexanderSashchenko <santys87@gmail.com>
2023-06-16 16:48:20 +0300

    Added logic improvements

--- src/main/java/com/example/promocodeService/service/PromocodeService.java
+++ src/main/java/com/example/promocodeService/service/PromocodeService.java
@@ -6,30 +6,21 @@ import com.example.promocodeService.model.Promocode;
 import com.example.promocodeService.model.Status;
 import com.example.promocodeService.repository.PromocodeRepository;
 import org.springframework.stereotype.Service;
-import reactor.core.Disposable;
 import reactor.core.publisher.Mono;
-import java.util.Objects;
 import java.util.Optional;
 
 @Service
 public class PromocodeService {
 
-    private final UserService userService;
     private final RequestHandler requestHandler;
     private final PromocodeRepository promocodeRepository;
 
-    public PromocodeService(UserService userService,
-                            PromocodeRepository promocodeRepository,
+    public PromocodeService(PromocodeRepository promocodeRepository,
                             RequestHandler requestHandler) {
-        this.userService = userService;
         this.requestHandler = requestHandler;
         this.promocodeRepository = promocodeRepository;
     }
 
-    /**
-     * Using .block() in non-blocking context for convenience as far as we are creating users on flight
-     * during promocode generation and not receiving userId from client
-     */
     public Mono<String> createPromocode(Long userId) {
         Optional<Promocode> promocodeOptional = promocodeRepository.findPromocodeByUserId(userId).blockOptional();
         if (promocodeOptional.isEmpty()) {
